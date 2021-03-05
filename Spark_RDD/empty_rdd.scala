e82859a9b19b1ed0a9456e5083ac93e7a9549cea
Saurav Kumar <72607485+Saurav3218@users.noreply.github.com>
2021-03-05 15:37:18 +0530

    Update empty_rdd.scala
    done.

--- Spark_RDD/empty_rdd.scala
+++ Spark_RDD/empty_rdd.scala
@@ -11,5 +11,12 @@ object CreateEmptyRdd extends App {
   println(rdd1)
   println(rddstring)
   println("Num of Partitions: "+rdd1.getNumPartitions)
+  rddstring.saveAsTextFile("/Users/SauravKumar/Desktop/empty_rdd2.txt")
+
+  // Pair RDD
+
+  type dataType = (String,Int)
+  var pairRDD = spark.sparkContext.emptyRDD[dataType]
+  println(pairRDD)
 
 }
